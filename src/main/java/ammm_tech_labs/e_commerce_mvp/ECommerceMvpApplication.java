648f44dc14e7728a71c77701024abbe38c2cc313
argosmaia <argosantao2013@gmail.com>
2026-02-10 09:44:37 -0300

    Enviando as primeiras versões do sistema, configurações, e enviroment

---
+++ src/main/java/ammm_tech_labs/e_commerce_mvp/ECommerceMvpApplication.java
@@ -0,0 +1,13 @@
+package ammm_tech_labs.e_commerce_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ammm_tech_labs/e_commerce_mvp/ECommerceMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package ammm_tech_labs.e_commerce_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
