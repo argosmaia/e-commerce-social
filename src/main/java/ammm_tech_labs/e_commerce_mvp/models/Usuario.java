8ddb7dd25bedf8a53630ca974f338c3d464e48a5
argosmaia <argosantao2013@gmail.com>
2026-02-18 22:01:08 -0300

    feat(criação): criação da model Usuario.java, criação de application.example.yaml e alguns cheatsheets

---
+++ src/main/java/ammm_tech_labs/e_commerce_mvp/models/Usuario.java
@@ -0,0 +1,29 @@
+package ammm_tech_labs.e_commerce_mvp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    
+    private String email;
+
+    private LocalDate dataNascimento;
+}
