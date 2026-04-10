1da14cfcc21140a5bb333433d1de65e4c7c2b152
Argos Maia <maia.argos@graduacao.uerj.br>
2026-04-10 13:12:40 -0300

    feat(db): enviando a primeira seed de validations db/V1__create_table_users.sql

--- src/main/java/ammm_tech_labs/e_commerce_mvp/seeders/UsuarioSeeder.java
+++ src/main/java/ammm_tech_labs/e_commerce_mvp/seeders/UsuarioSeeder.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Component;
-
-import ammm_tech_labs.e_commerce_mvp.dto.create.CriarUsuarioDTO;
 import ammm_tech_labs.e_commerce_mvp.factory.UsuarioFactory;
 import ammm_tech_labs.e_commerce_mvp.maps.UsuarioMapper;
 import ammm_tech_labs.e_commerce_mvp.models.Usuario;
@@ -17,18 +15,18 @@ public class UsuarioSeeder {
     private final UsuarioFactory usuarioFactory;
     private final UsuarioMapper usuarioMapper;
 
-    public UsuarioSeeder(UsuarioRepository usuarioRepository, UsuarioFactory usuarioFactory, UsuarioMapper usuarioMapper) {
+    public UsuarioSeeder(UsuarioRepository usuarioRepository, UsuarioFactory usuarioFactory,
+            UsuarioMapper usuarioMapper) {
         this.usuarioRepository = usuarioRepository;
         this.usuarioFactory = usuarioFactory;
         this.usuarioMapper = usuarioMapper;
     }
 
     public void run() {
-        List<Usuario> usuarios = IntStream.range(0,10)
-            .mapToObj(i -> usuarioMapper.toEntity(
-                usuarioFactory.criarUsuarioDTOAleatorio()
-            ))
-            .toList();
+        List<Usuario> usuarios = IntStream.range(0, 10)
+                .mapToObj(i -> usuarioMapper.toEntity(
+                        usuarioFactory.criarUsuarioDTOAleatorio()))
+                .toList();
 
         usuarioRepository.saveAll(usuarios);
 
